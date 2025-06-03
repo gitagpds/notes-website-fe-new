7a0a118acaf1aa665a2056ca0d6f5c51bbf8de19
Gita Poetri Dewi Siregar <146951904+gitagpds@users.noreply.github.com>
2025-06-03 14:09:42 +0700

    Update homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -50,9 +50,9 @@ const HomePage = () => {
       <div
         className="container-fluid p-4 main-content"
         style={{
-          marginLeft: "280px",
-          minHeight: "100vh",
-          maxWidth: "700px",
+          marginLeft: "100px",
+          minHeight: "1000vh",
+          maxWidth: "7000px",
           paddingLeft: "40px",
           paddingTop: "60px",
         }}
@@ -60,18 +60,18 @@ const HomePage = () => {
         <div className="row align-items-center">
           <div
             className="col-md-6 text-center text-md-start main-content-text"
-            style={{ maxWidth: "400px" }}
+            style={{ maxWidth: "4000px" }}
           >
-            <p className="lead" style={{ fontSize: "15px", fontWeight: "bold", color: "rgba(0, 0, 0, 0.616)" }}>
+            <p className="lead" style={{ fontSize: "150px", fontWeight: "bold", color: "rgba(0, 0, 0, 0.616)" }}>
               <strong>
                 Memomate helps you organize and take notes efficiently. Start writing now to keep your thoughts structured.
               </strong>
             </p>
-            <Link to="/add-note" className="btn btn-lg btn-purple text-white" style={{ fontSize: "15px", padding: "8px 18px" }}>
+            <Link to="/add-note" className="btn btn-lg btn-purple text-white" style={{ fontSize: "150px", padding: "80px 180px" }}>
               + Note-taking
             </Link>
           </div>
-          <div className="col-md-6 text-center" style={{ marginTop: "20px" }}>
+          <div className="col-md-6 text-center" style={{ marginTop: "200px" }}>
             <img
               src={homepageImg}
               alt="Homepage Illustration"
